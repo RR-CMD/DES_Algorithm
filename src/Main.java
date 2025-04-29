2aa11c43e6d5c6eb583d8d3a6d5171a4f2ff274f
Osama Mulhem <osamayousefosama@gmail.com>
2025-04-29 17:54:06 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,6 @@ public class Main {
         String plainText = decryptionExample.decrypt();
         decryptionExample.printDecryption(plainText);
 
-
-
-
-
         DES encryptionExample2 = new DES("0001001100110100010101110111100110011011101111001101111111110001","0000000100100011010001010110011110001001101010111100110111101111",true);
         String cipherText2 = encryptionExample2.encrypt();
         encryptionExample2.printEncryption(cipherText2);
@@ -27,14 +23,13 @@ public class Main {
 
 
 
-        DES encryptionExample3 = new DES("TESTTEST",    "TESTTEST"         ,false);
+        DES encryptionExample3 = new DES("TESTTESTTT",    "TESTTEST"         ,false);
         String cipherText3 = encryptionExample3.encrypt();
         encryptionExample3.printEncryption(cipherText3);
 
-        DES decryptionExample3 = new DES("TESTTEST", DES_Utility.binaryToString(cipherText3),false);
+        DES decryptionExample3 = new DES("0001001100110100010101110111100110011011101111001101111111110001", "00010011001101000101011101111001100110111011110011011111111100011",false);
         String plainText3 = decryptionExample3.decrypt();
         decryptionExample3.printDecryption(plainText3);
-
 
 
 
